abab794ace060e1a0c329ac99b5329ce87df9517
AnWang <AnWang@dyn-160-39-250-237.dyn.columbia.edu>
2016-12-08 17:17:12 -0500

    aaa

--- trading_system/dashboard/views.py
+++ trading_system/dashboard/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.template import Context, loader
 from django.shortcuts import render_to_response
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # from orders.models import Order
 
 # Create your views here.
+@login_required
 def dashboard(request):
 	# template = loader.get_template('dashboard/dashboard.html')
 	# return HttpResponse(template.render())
@@ -20,6 +22,10 @@ def dashboard(request):
 	# 	data.append({'company_name': o.company_name, 'quantity': o.quantity})
 
 	# return render(request, 'dashboard/dashboard.html', {'apple': str(data)})
+	print ("***********")
+	username = request.user.username
+	userid = request.user.id
+	return render_to_response('dashboard/dashboard.html', locals())
 	return render(request, 'dashboard/dashboard.html')
 
 def test(request):
--- trading_system/trading_system/settings.py
+++ trading_system/trading_system/settings.py
@@ -120,6 +120,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
