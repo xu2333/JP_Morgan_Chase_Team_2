b1f2640408743b0b8d3c1d4f4f1109cc3a2a4de1
mw10104587 <mw10104587@gmail.com>
2016-12-12 11:18:41 -0500

    merge bar chart

--- trading_system/dashboard/static/dashboard.js
+++ trading_system/dashboard/static/dashboard.js
@@ -362,13 +362,11 @@ JPTrader.initWebSocket = function( callback ){
           if ( next["message_type"] == "sold_message" ) return agg + parseInt(next["sold_quantity"]);
           return agg;
         } ,0);
+
         // doesn't look really reliable.
         const firstTimeStamp = receivedMessage[0]["timestamp"];
         // const series1 = JPTrader.quoteChart.series[1];
         const series1 = JPTrader.volumnChart.series[0];
-
-        // console.log('add point');
-        // console.log(`first timestamp: ${firstTimeStamp}, sold sum: ${soldSum}`);
         series1.addPoint( [ (new Date(firstTimeStamp)).getTime(), soldSum ], true, true);
         
       }
@@ -383,7 +381,7 @@ JPTrader.initWebSocket = function( callback ){
           case "quote":
 
             JPTrader.quoteData.push( message["quote"] );
-            // console.log(message.timestamp);
+            console.log(message);
 
             // Execute on first received quote
             // draw chart and set interval for updating delta.
@@ -415,6 +413,9 @@ JPTrader.initWebSocket = function( callback ){
               // console.log(`in this part with timestamp: ${ (new Date(message["timestamp"])).getTime() }`);
               const series0 = JPTrader.quoteChart.series[0];
               series0.addPoint( [ (new Date(message["timestamp"])).getTime(), parseFloat(message["quote"]) ], true, true);
+
+              const series1 = JPTrader.volumnChart.series[0];
+              series1.addPoint( [ (new Date(message["timestamp"])).getTime(), parseFloat(message["size"]) ], true, true);
             
             }
 
@@ -792,213 +793,213 @@ $(function () {
   });
 
 
-  Highcharts.createElement('link', {
-    href: 'https://fonts.googleapis.com/css?family=Unica+One',
-    rel: 'stylesheet',
-    type: 'text/css'
-  }, null, document.getElementsByTagName('head')[0]);
+  // Highcharts.createElement('link', {
+  //   href: 'https://fonts.googleapis.com/css?family=Unica+One',
+  //   rel: 'stylesheet',
+  //   type: 'text/css'
+  // }, null, document.getElementsByTagName('head')[0]);
 
-  Highcharts.theme = {
-     colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
-        '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
-     chart: {
-        backgroundColor: {
-           linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-           stops: [
-              [0, '#2a2a2b'],
-              [1, '#3e3e40']
-           ]
-        },
-        style: {
-           fontFamily: '\'Unica One\', sans-serif'
-        },
-        plotBorderColor: '#606063'
-     },
-     title: {
-        style: {
-           color: '#E0E0E3',
-           textTransform: 'uppercase',
-           fontSize: '20px'
-        }
-     },
-     subtitle: {
-        style: {
-           color: '#E0E0E3',
-           textTransform: 'uppercase'
-        }
-     },
-     xAxis: {
-        gridLineColor: '#707073',
-        labels: {
-           style: {
-              color: '#E0E0E3'
-           }
-        },
-        lineColor: '#707073',
-        minorGridLineColor: '#505053',
-        tickColor: '#707073',
-        title: {
-           style: {
-              color: '#A0A0A3'
+  // Highcharts.theme = {
+  //    colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
+  //       '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+  //    chart: {
+  //       backgroundColor: {
+  //          linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+  //          stops: [
+  //             [0, '#2a2a2b'],
+  //             [1, '#3e3e40']
+  //          ]
+  //       },
+  //       style: {
+  //          fontFamily: '\'Unica One\', sans-serif'
+  //       },
+  //       plotBorderColor: '#606063'
+  //    },
+  //    title: {
+  //       style: {
+  //          color: '#E0E0E3',
+  //          textTransform: 'uppercase',
+  //          fontSize: '20px'
+  //       }
+  //    },
+  //    subtitle: {
+  //       style: {
+  //          color: '#E0E0E3',
+  //          textTransform: 'uppercase'
+  //       }
+  //    },
+  //    xAxis: {
+  //       gridLineColor: '#707073',
+  //       labels: {
+  //          style: {
+  //             color: '#E0E0E3'
+  //          }
+  //       },
+  //       lineColor: '#707073',
+  //       minorGridLineColor: '#505053',
+  //       tickColor: '#707073',
+  //       title: {
+  //          style: {
+  //             color: '#A0A0A3'
 
-           }
-        }
-     },
-     yAxis: {
-        gridLineColor: '#707073',
-        labels: {
-           style: {
-              color: '#E0E0E3'
-           }
-        },
-        lineColor: '#707073',
-        minorGridLineColor: '#505053',
-        tickColor: '#707073',
-        tickWidth: 1,
-        title: {
-           style: {
-              color: '#A0A0A3'
-           }
-        }
-     },
-     tooltip: {
-        backgroundColor: 'rgba(0, 0, 0, 0.85)',
-        style: {
-           color: '#F0F0F0'
-        }
-     },
-     plotOptions: {
-        series: {
-           dataLabels: {
-              color: '#B0B0B3'
-           },
-           marker: {
-              lineColor: '#333'
-           }
-        },
-        boxplot: {
-           fillColor: '#505053'
-        },
-        candlestick: {
-           lineColor: 'white'
-        },
-        errorbar: {
-           color: 'white'
-        }
-     },
-     legend: {
-        itemStyle: {
-           color: '#E0E0E3'
-        },
-        itemHoverStyle: {
-           color: '#FFF'
-        },
-        itemHiddenStyle: {
-           color: '#606063'
-        }
-     },
-     credits: {
-        style: {
-           color: '#666'
-        }
-     },
-     labels: {
-        style: {
-           color: '#707073'
-        }
-     },
+  //          }
+  //       }
+  //    },
+  //    yAxis: {
+  //       gridLineColor: '#707073',
+  //       labels: {
+  //          style: {
+  //             color: '#E0E0E3'
+  //          }
+  //       },
+  //       lineColor: '#707073',
+  //       minorGridLineColor: '#505053',
+  //       tickColor: '#707073',
+  //       tickWidth: 1,
+  //       title: {
+  //          style: {
+  //             color: '#A0A0A3'
+  //          }
+  //       }
+  //    },
+  //    tooltip: {
+  //       backgroundColor: 'rgba(0, 0, 0, 0.85)',
+  //       style: {
+  //          color: '#F0F0F0'
+  //       }
+  //    },
+  //    plotOptions: {
+  //       series: {
+  //          dataLabels: {
+  //             color: '#B0B0B3'
+  //          },
+  //          marker: {
+  //             lineColor: '#333'
+  //          }
+  //       },
+  //       boxplot: {
+  //          fillColor: '#505053'
+  //       },
+  //       candlestick: {
+  //          lineColor: 'white'
+  //       },
+  //       errorbar: {
+  //          color: 'white'
+  //       }
+  //    },
+  //    legend: {
+  //       itemStyle: {
+  //          color: '#E0E0E3'
+  //       },
+  //       itemHoverStyle: {
+  //          color: '#FFF'
+  //       },
+  //       itemHiddenStyle: {
+  //          color: '#606063'
+  //       }
+  //    },
+  //    credits: {
+  //       style: {
+  //          color: '#666'
+  //       }
+  //    },
+  //    labels: {
+  //       style: {
+  //          color: '#707073'
+  //       }
+  //    },
 
-     drilldown: {
-        activeAxisLabelStyle: {
-           color: '#F0F0F3'
-        },
-        activeDataLabelStyle: {
-           color: '#F0F0F3'
-        }
-     },
+  //    drilldown: {
+  //       activeAxisLabelStyle: {
+  //          color: '#F0F0F3'
+  //       },
+  //       activeDataLabelStyle: {
+  //          color: '#F0F0F3'
+  //       }
+  //    },
 
-     navigation: {
-        buttonOptions: {
-           symbolStroke: '#DDDDDD',
-           theme: {
-              fill: '#505053'
-           }
-        }
-     },
+  //    navigation: {
+  //       buttonOptions: {
+  //          symbolStroke: '#DDDDDD',
+  //          theme: {
+  //             fill: '#505053'
+  //          }
+  //       }
+  //    },
 
-     // scroll charts
-     rangeSelector: {
-        buttonTheme: {
-           fill: '#505053',
-           stroke: '#000000',
-           style: {
-              color: '#CCC'
-           },
-           states: {
-              hover: {
-                 fill: '#707073',
-                 stroke: '#000000',
-                 style: {
-                    color: 'white'
-                 }
-              },
-              select: {
-                 fill: '#000003',
-                 stroke: '#000000',
-                 style: {
-                    color: 'white'
-                 }
-              }
-           }
-        },
-        inputBoxBorderColor: '#505053',
-        inputStyle: {
-           backgroundColor: '#333',
-           color: 'silver'
-        },
-        labelStyle: {
-           color: 'silver'
-        }
-     },
+  //    // scroll charts
+  //    rangeSelector: {
+  //       buttonTheme: {
+  //          fill: '#505053',
+  //          stroke: '#000000',
+  //          style: {
+  //             color: '#CCC'
+  //          },
+  //          states: {
+  //             hover: {
+  //                fill: '#707073',
+  //                stroke: '#000000',
+  //                style: {
+  //                   color: 'white'
+  //                }
+  //             },
+  //             select: {
+  //                fill: '#000003',
+  //                stroke: '#000000',
+  //                style: {
+  //                   color: 'white'
+  //                }
+  //             }
+  //          }
+  //       },
+  //       inputBoxBorderColor: '#505053',
+  //       inputStyle: {
+  //          backgroundColor: '#333',
+  //          color: 'silver'
+  //       },
+  //       labelStyle: {
+  //          color: 'silver'
+  //       }
+  //    },
 
-     navigator: {
-        handles: {
-           backgroundColor: '#666',
-           borderColor: '#AAA'
-        },
-        outlineColor: '#CCC',
-        maskFill: 'rgba(255,255,255,0.1)',
-        series: {
-           color: '#7798BF',
-           lineColor: '#A6C7ED'
-        },
-        xAxis: {
-           gridLineColor: '#505053'
-        }
-     },
+  //    navigator: {
+  //       handles: {
+  //          backgroundColor: '#666',
+  //          borderColor: '#AAA'
+  //       },
+  //       outlineColor: '#CCC',
+  //       maskFill: 'rgba(255,255,255,0.1)',
+  //       series: {
+  //          color: '#7798BF',
+  //          lineColor: '#A6C7ED'
+  //       },
+  //       xAxis: {
+  //          gridLineColor: '#505053'
+  //       }
+  //    },
 
-     scrollbar: {
-        barBackgroundColor: '#808083',
-        barBorderColor: '#808083',
-        buttonArrowColor: '#CCC',
-        buttonBackgroundColor: '#606063',
-        buttonBorderColor: '#606063',
-        rifleColor: '#FFF',
-        trackBackgroundColor: '#404043',
-        trackBorderColor: '#404043'
-     },
+  //    scrollbar: {
+  //       barBackgroundColor: '#808083',
+  //       barBorderColor: '#808083',
+  //       buttonArrowColor: '#CCC',
+  //       buttonBackgroundColor: '#606063',
+  //       buttonBorderColor: '#606063',
+  //       rifleColor: '#FFF',
+  //       trackBackgroundColor: '#404043',
+  //       trackBorderColor: '#404043'
+  //    },
 
-     // special colors for some of the
-     legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
-     background2: '#505053',
-     dataLabelsColor: '#B0B0B3',
-     textColor: '#C0C0C0',
-     contrastTextColor: '#F0F0F3',
-     maskColor: 'rgba(255,255,255,0.3)'
-  };
+  //    // special colors for some of the
+  //    legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+  //    background2: '#505053',
+  //    dataLabelsColor: '#B0B0B3',
+  //    textColor: '#C0C0C0',
+  //    contrastTextColor: '#F0F0F3',
+  //    maskColor: 'rgba(255,255,255,0.3)'
+  // };
 
-  // Apply the theme
-  JPTrader.quoteChart.setOptions(Highcharts.theme);
+  // // Apply the theme
+  // JPTrader.quoteChart.setOptions(Highcharts.theme);
 
 
 
@@ -1190,7 +1191,7 @@ A function to setup all the interactions, linkage and connections
 */
 JPTrader.init = function(){
 
-  console.log('in init');
+  // console.log('in init');
   console.log(document.getElementById("make-order-btn"));
   if ( document.getElementById("make-order-btn") === null ) return;
 
